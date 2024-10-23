07ca0ca595b7cf0b09e4570d3fb163f09facfe0f
Oussama Elhousni <oussamaelhousnioe@gmail.com>
2024-10-23 22:06:22 +0100

    init frontend app

---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+function Navbar() {
+  return (
+    <div className="bg-zinc-900">
+      <div className="max-w-[1100px] mx-auto py-4">
+        <h2 className="text-xl font-semibold text-white">File manager</h2>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
